fdcbbb9b7a336f028e28744f2668b9285b15f42d
Manuel Winter <manuel@manuelwinter.eu>
2025-04-23 23:36:52 +0200

    Initial Commit

---
+++ StructuredQueryMacroBug/StructuredQueryMacroBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuredQueryMacroBugApp.swift
+//  StructuredQueryMacroBug
+//
+//  Created by Manuel Winter on 23.04.25.
+//
+
+import SwiftUI
+
+@main
+struct StructuredQueryMacroBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
